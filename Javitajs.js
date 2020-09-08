42bce61b01596c0490b7dd45d0ce6209401d4830
Agustin Donnetta <agu.donnetta@gmail.com>
2020-09-08 02:39:59 -0300

    Prompt exercise

---
+++ Javitajs.js
@@ -0,0 +1,6 @@
+var firstName = prompt ("What is your frst name?")
+var LastName = prompt ("What is your last name?")
+var age = prompt ("What is your age?")
+
+console.log (`Your full name is  ${firstName} ${LastName} `)
+console.log (`You are ${age} years old`)
